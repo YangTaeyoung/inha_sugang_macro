b3390c6990cb5b0a78921ab40a8e7403dfdf1690
--replace-all <--replace-all>
2022-02-12 17:05:41 +0900

    효율 개선

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 import datetime
 
 
+# 크롬 드라이버를 세팅하고, 실행시키는 함수
 def set_chrome_driver(headless=False):
     chrome_options = webdriver.ChromeOptions()
     if headless:
@@ -15,13 +16,13 @@ def set_chrome_driver(headless=False):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
 
-
+# 현재 날짜를 가져오는 함수
 def get_now(driver_time):
     text = driver_time.find_element(by=By.ID, value="time_area").text
     time = datetime.datetime.strptime(text, "%Y년 %m월 %d일 %H시 %M분 %S초")
     return time
 
-
+# 로그인 시키는 함수, 비밀번호가 틀리면 다시 입력하라는 함수가 출력
 def login(driver):
     while True:
         # 아이디, 비밀번호 입력 받는 곳
@@ -57,19 +58,22 @@ def wait_alert(driver):
 def try_sugang(driver):
     for i in range(1, len(driver.find_elements(by=By.XPATH, value='//*[@id="dgList2"]/tbody//*')) // 11 + 1):
         try:
-            driver.find_element(by=By.XPATH,
-                                value=f"/html/body/form/div[2]/div[2]/div[4]/table/tbody/tr[{i}]/td[9]/input").click()
-        except:
-            continue
-        # 수강신청 하시겠습니까? alert창 무효화
-        wait_alert(driver)
-        print("수강신청 질문 창 무효화")
-        # 수강신청이 성공되었습니다./인원이 가득 찼습니다. alert창 무효화
-        wait_alert(driver)
-        print("수강신청 성공/실패 창 무효화")
+            # title = driver.find_element(by=By.XPATH,
+            #                             value=f"/html/body/form/div[2]/div[2]/div[4]/table/tbody/tr[{i}]/td[2]/").text
+            print(f"INFO: title 에 수강신청을 시도합니다.")
+            button = driver.find_element(by=By.XPATH,
+                                         value=f"/html/body/form/div[2]/div[2]/div[4]/table/tbody/tr[{i}]/td[9]/input")
+            button.click()
+            # 수강신청 하시겠습니까? alert창 무효화
+            wait_alert(driver)
+            print("INFO: 수강신청 질문 창을 무효화 합니다.")
+            # 수강신청이 성공되었습니다./인원이 가득 찼습니다. alert창 무효화
+            print(f"INFO: title: {wait_alert(driver)}")
+        except:  # 오류 발견 시: 즉시 종료 처리
+            return
 
 
-USER_MODE = False
+USER_MODE = True
 
 if USER_MODE:
     print("################################################################################")
@@ -86,14 +90,10 @@ if USER_MODE:
     if flag.lower() != "y":
         sys.exit()
     print("################################################################################")
-    print("백그라운드에서 실행하실 수 있습니다. 크롬창이 보이지 않습니다만, \n 해당기능은 테스트가 완전히 실행되지 않았습니다.")
-    print("따라서 불안정하게 동작할 수 있습니다.")
-    print("단, 백그라운드에서 실행하실 경우 하시던 작업을 지속하실 수 있습니다.")
-    print("제작자는 2개의 컴퓨터를 가진 경우 컴퓨터 하나에는 메크로를 백그라운드가 아닌 경우\n로 실행시키고, 본 컴퓨터에서 실제 수강신청을 하길 권장합니다.")
-    flag = input("백 그라운드에서 실행하시겠습니까? (Y/N): ")
+    print("해당 화면은 콘솔창만 보이게 됩니다.")
+    print("실제로 창은 보이지 않으나, 수강신청을 자동으로 시도합니다.")
+    print("백그라운드에서 작업을 시도하므로 다른 작업을 하셔도 무관합니다.")
     print("################################################################################")
-    print("해당 버전은 Beta 버전입니다.")
-    print("오류가 발생할 수 있으며,")
     print("해당 프로그램에 의해 발생한 피해(수강신청 실패 등)은 제작자가 책임지지 않습니다.")
     print("################################################################################")
 
@@ -106,23 +106,14 @@ if USER_MODE:
     start_time = datetime.datetime.combine(today, times)
     print(f"설정된 시각: {start_time.isoformat()}")
 
-    if flag.lower() == "y":
-        headless = True
-    else:
-        headless = False
 # 전체 수강신청 흐름 담당
-driver = set_chrome_driver(headless=False)
+driver = set_chrome_driver(headless=True)
 # 원격으로 시간 정보 받아오는 담당
 driver_time = set_chrome_driver(headless=True)
 # 수강 신청 사이트 오픈 (유저에게 보이게 설정)
 driver.get("https://sugang.inha.ac.kr/sugang/")
 # 시간 보는 사이트 오픈 (유저에게 보이지 않게 설정)
 driver_time.get("https://time.navyism.com/?host=sugang.inha.ac.kr")
-
-    # while True:
-    #     if (rest := (start_time - get_now(driver_time))).seconds <= 0:
-    #         break
-    #     print(f"남은 시간: {rest.seconds}초")
 
 # 프레임 이동
 driver.switch_to.frame("ifrm")
@@ -132,17 +123,22 @@ driver.implicitly_wait(1)
 # 로그인
 login(driver)
 
+while True:
+    if (rest := (start_time - get_now(driver_time))).seconds <= 0:
+        break
+    print(f"남은 시간: {rest.seconds}초")
+
 # 수강신청 페이지로 이동
 driver.execute_script('moveUrl("SU_53001/Sugang_Save.ASPX")')
 driver.switch_to.parent_frame()
 driver.switch_to.frame("MainFrame")
 
 # 수강신청 페이지에서 첫 alert창 기다렸다 무효화
-print(wait_alert(driver))
+wait_alert(driver)
 print("INFO: 첫 알림 메시지를 무효화 합니다.")
 # 수강신청 대기 리스트가 있으면 수강신청 시도 계속하기
 while count := (len(driver.find_elements(by=By.XPATH, value='//*[@id="dgList2"]/tbody//*')) // 11):
-    print(f"수강신청 항목이 {count}개 남았습니다. 계속 시도합니다.")
+    print(f"INFO: 수강신청 항목이 {count}개 남았습니다. 계속 시도합니다.")
     try_sugang(driver)
 
-print("수강신청이 종료되었습니다.")
+print("수강신청이 성공적으로 종료되었습니다. 축하합니다.")
