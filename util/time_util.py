b5535d7a92bf22871670d010bcd36615c4ddfda4
Taeyoung Yang <0130yang@gmail.com>
2022-02-12 22:03:31 +0900

    [Refactor] 타이머 코드 리펙토링

--- util/time_util.py
+++ util/time_util.py
@@ -1,4 +1,6 @@
 import datetime
+from .crawler import get_now
+from .console import console_clear
 
 
 def get_diff_time_as_seconds(time1: datetime.datetime, time2: datetime.datetime):
@@ -12,3 +14,11 @@ def print_rest_time(seconds: int):
     minute = seconds % 3600 // 60
     second = seconds % 60
     print(f"남은시간: {day}일 {hour}시간 {minute}분 {second}초")
+
+
+def go_timer(start_time: datetime.datetime, time_browser):
+    while True:
+        if (seconds := get_diff_time_as_seconds(start_time, get_now(time_browser))) <= 0:
+            break
+        console_clear()
+        print_rest_time(seconds)
